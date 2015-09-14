eee01540146e801ffcdec43379ecd7a2827b6304
周卓 <yecan.xyc@alibaba-inc.com>
2015-09-14 14:31:31 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## dpa-demo-ios
+
+### httpdns-ios-demo
+
+运行Demo前请前在`ViewController.m`文件中填入您的账号信息：
+
+```
+NSString * testAk = @"*********";
+NSString * testSk = @"**********";
+NSString * testAppid = @"**********";
+```
---
+++ httpdns_ios_demo/httpdns_ios_demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  httpdns_ios_demo
+//
+//  Created by zhouzhuo on 9/14/15.
+//  Copyright (c) 2015 zhouzhuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
