20f27df219167ded725449cdf91f88af5ddaf5f8
nanpo.yhl <nanpo.yhl@alibaba-inc.com>
2015-10-30 16:22:32 +0800

    增加httpdns_restful_demo

---
+++ httpdns_restful_demo/HttpDNSApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HttpDNSApp
+//
+//  Created by nanpo.yhl on 15/10/30.
+//  Copyright © 2015年 com.aliyun.mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ httpdns_restful_demo/httpdns_restful_demoTests/httpdns_restful_demoTests.m
@@ -0,0 +1,52 @@
+//
+//  httpdns_restful_demoTests.m
+//  httpdns_restful_demoTests
+//
+//  Created by nanpo.yhl on 15/10/29.
+//  Copyright © 2015年 com.aliyun.mobile. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#include "HttpDNS.h"
+#import "NetworkManager.h"
+
+@interface httpdns_restful_demoTests : XCTestCase
+
+@end
+
+@implementation httpdns_restful_demoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+-(void)testSingleHost {
+    HttpDNS* instance = [HttpDNS instance];
+    
+    NSString* ip = [instance getIpByHost:@"gw.alicdn.com"];
+    
+    NSLog(@" ip %@", ip);
+    
+    NSString* netSting = [[NetworkManager instance] currentWifiSsid];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
