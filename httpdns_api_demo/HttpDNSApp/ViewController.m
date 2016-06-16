dee9a7a22dea96651cb5f4f4cdd2541354303feb
禅赫 <xanxusiou@163.com>
2016-06-16 16:55:31 +0800

    modify the https validate of nsurlconnection

--- httpdns_api_demo/HttpDNSApp/ViewController.m
+++ httpdns_api_demo/HttpDNSApp/ViewController.m
@@ -46,14 +46,7 @@
         }
     }
     
-//    NSHTTPURLResponse* response;
-//    Method originalMethod=class_getInstanceMethod([NSURLConnection class], @selector(connection:willSendRequestForAuthenticationChallenge:));
-//    Method swappedMethod=class_getInstanceMethod([NSURLConnection class], @selector(hda_connection:willSendRequestForAuthenticationChallenge:));
-//    method_exchangeImplementations(originalMethod, swappedMethod);
     NSURLConnection* connection=[[NSURLConnection alloc] initWithRequest:_request delegate:self startImmediately:YES];
-//    NSData* data = [NSURLConnection sendSynchronousRequest:self.request returningResponse:&response error:nil];
-    
-//    NSLog(@"response %@",response);
     
 }
 
@@ -82,75 +75,6 @@
     return NO;
 }
 
--(void)connection:(NSURLConnection *)connection willSendRequestForAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge{
-    if (!challenge) {
-        return;
-    }
-    
-    /*
-     * URL里面的host在使用HTTPDNS的情况下被设置成了IP，此处从HTTP Header中获取真实域名
-     */
-    NSString* host = [[self.request allHTTPHeaderFields] objectForKey:@"host"];
-    if (!host) {
-        host = self.request.URL.host;
-    }
-    
-    /*
-     * 判断challenge的身份验证方法是否是NSURLAuthenticationMethodServerTrust（HTTPS模式下会进行该身份验证流程），
-     * 在没有配置身份验证方法的情况下进行默认的网络请求流程。
-     */
-    if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust])
-    {
-        if ([self evaluateServerTrust:challenge.protectionSpace.serverTrust forDomain:host]) {
-            /*
-             * 验证完以后，需要构造一个NSURLCredential发送给发起方
-             */
-            NSURLCredential *credential = [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust];
-            [[challenge sender] useCredential:credential forAuthenticationChallenge:challenge];
-        } else {
-//            /*
-//             * 验证失败，取消这次验证流程
-//             */
-            [[challenge sender] cancelAuthenticationChallenge:challenge];
-        }
-    } else {
-        /*
-         * 对于其他验证方法直接进行处理流程
-         */
-        [[challenge sender] continueWithoutCredentialForAuthenticationChallenge:challenge];
-    }
-}
-- (BOOL)evaluateServerTrust:(SecTrustRef)serverTrust
-                  forDomain:(NSString *)domain
-{
-//    domain=@"book.douban.com";
-    /*
-     * 创建证书校验策略
-     */
-    NSMutableArray *policies = [NSMutableArray array];
-    if (domain) {
-        [policies addObject:(__bridge_transfer id)SecPolicyCreateSSL(true, (__bridge CFStringRef)domain)];
-    } else {
-        [policies addObject:(__bridge_transfer id)SecPolicyCreateBasicX509()];
-    }
-    
-    /*
-     * 绑定校验策略到服务端的证书上
-     */
-    SecTrustSetPolicies(serverTrust, (__bridge CFArrayRef)policies);
-    
-    
-    /*
-     * 评估当前serverTrust是否可信任，
-     * 官方建议在result = kSecTrustResultUnspecified 或 kSecTrustResultProceed
-     * 的情况下serverTrust可以被验证通过，https://developer.apple.com/library/ios/technotes/tn2232/_index.html
-     * 关于SecTrustResultType的详细信息请参考SecTrust.h
-     */
-    SecTrustResultType result;
-    SecTrustEvaluate(serverTrust, &result);
-    
-    return (result == kSecTrustResultUnspecified || result == kSecTrustResultProceed);
-}
 -(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data{
     NSLog(@"receive data:%@",[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
 }
