c16105b1b170da8dca57db5f6462eebd36057b36
lingkun.lk <lingkun.lk@alibaba-inc.com>
2017-04-24 11:49:17 +0800

    Update HTTPDNS SDK to v1.5.0

--- httpdns_ios_demo/AlicloudHttpDNS.framework/Headers/AlicloudHttpDNS.h
+++ httpdns_ios_demo/AlicloudHttpDNS.framework/Headers/AlicloudHttpDNS.h
@@ -19,7 +19,7 @@
 
 #import <Foundation/Foundation.h>
 
-static NSString *const HTTPDNS_IOS_SDK_VERSION = @"1.4.2";
+static NSString *const HTTPDNS_IOS_SDK_VERSION = @"1.5.0";
 
 #import <Foundation/Foundation.h>
 #import "HttpdnsServiceProvider.h"
