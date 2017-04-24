0d97e9819baf84e2ac6eed6a092af3972939e28c
lingkun.lk <lingkun.lk@alibaba-inc.com>
2017-04-24 11:47:34 +0800

    Update HTTPDNS to v1.4.2

--- httpdns_ios_demo/AlicloudHttpDNS.framework/Headers/AlicloudHttpDNS.h
+++ httpdns_ios_demo/AlicloudHttpDNS.framework/Headers/AlicloudHttpDNS.h
@@ -18,7 +18,9 @@
  */
 
 #import <Foundation/Foundation.h>
+
+static NSString *const HTTPDNS_IOS_SDK_VERSION = @"1.4.2";
+
+#import <Foundation/Foundation.h>
 #import "HttpdnsServiceProvider.h"
 #import "HttpdnsDegradationDelegate.h"
-
-static NSString *const HTTPDNS_IOS_SDK_VERSION = @"1.4.1";
