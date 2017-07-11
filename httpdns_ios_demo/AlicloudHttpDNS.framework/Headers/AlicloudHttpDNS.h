6c75e6512fd5da3f7c3ceb85975a56fbc0e3f6fd
Elon Chan <luohanchenyilong@163.com>
2017-07-11 19:26:28 +0800

    HTTPDNS update to 1.5.3

--- httpdns_ios_demo/AlicloudHttpDNS.framework/Headers/AlicloudHttpDNS.h
+++ httpdns_ios_demo/AlicloudHttpDNS.framework/Headers/AlicloudHttpDNS.h
@@ -19,7 +19,7 @@
 
 #import <Foundation/Foundation.h>
 
-static NSString *const HTTPDNS_IOS_SDK_VERSION = @"1.5.2";
+static NSString *const HTTPDNS_IOS_SDK_VERSION = @"1.5.3";
 
 #import <Foundation/Foundation.h>
 #import "HttpdnsServiceProvider.h"
