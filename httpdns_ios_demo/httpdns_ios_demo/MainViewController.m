ca5abbe1b4dd47284200dc37223c8ddcf5782cfd
周卓 <yecan.xyc@alibaba-inc.com>
2024-04-30 21:48:39 +0800

    feat: 调整gitignore和httpdnsdemo配置

--- httpdns_ios_demo/httpdns_ios_demo/MainViewController.m
+++ httpdns_ios_demo/httpdns_ios_demo/MainViewController.m
@@ -12,6 +12,7 @@
 #import "HTTPSScene.h"
 #import <AlicloudHttpDNS/AlicloudHttpDNS.h>
 
+
 @interface MainViewController ()
 @property (weak, nonatomic) IBOutlet UITextView *textView;
 
@@ -21,8 +22,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    
 }
 
 - (IBAction)beginCommonScenceQuery:(id)sender {
@@ -79,17 +78,6 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
-
-/*
- #pragma mark - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
- }
- */
 
 @end
