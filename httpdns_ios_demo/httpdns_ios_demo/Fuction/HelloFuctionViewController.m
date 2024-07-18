bc4525e6954af244f30c5e6c444640c731eff4bc
任志清 <dabai0424@gmail.com>
2024-07-18 15:56:58 +0800

    feat: 解析结果页完成

--- httpdns_ios_demo/httpdns_ios_demo/Fuction/HelloFuctionViewController.m
+++ httpdns_ios_demo/httpdns_ios_demo/Fuction/HelloFuctionViewController.m
@@ -8,14 +8,18 @@
 #import "HelloFuctionViewController.h"
 #import "ResolveTypeButton.h"
 #import "ChooseOrInputDomainViewController.h"
+#import "ResolveFuctionViewController.h"
 
 @interface HelloFuctionViewController ()<ChooseOrInputDomainDelegate>
 
-@property (weak, nonatomic) IBOutlet UITextField *domainsTextField;
+@property (weak, nonatomic) IBOutlet UITextField *domainTextField;
 @property (weak, nonatomic) IBOutlet ResolveTypeButton *syncResolveButton;
 @property (weak, nonatomic) IBOutlet ResolveTypeButton *asyncResolveButton;
-@property (weak, nonatomic) IBOutlet ResolveTypeButton *syncResolveNonBlockingButton;
+@property (weak, nonatomic) IBOutlet ResolveTypeButton *syncNonBlockingResolveButton;
+@property (weak, nonatomic) IBOutlet UILabel *resolveTypeExplanation;
 @property (weak, nonatomic) IBOutlet UIButton *resolveButton;
+
+@property(nonatomic, assign)ResolveType resolveType;
 
 @end
 
@@ -27,7 +31,7 @@
 }
 
 - (void)updateResolveButtonState {
-    if ([HTTPDNSDemoTools isValidString:self.domainsTextField.text]) {
+    if ([HTTPDNSDemoTools isValidString:self.domainTextField.text]) {
         [self.resolveButton setEnabled:YES];
     }
 }
@@ -37,19 +41,38 @@
 - (IBAction)syncButton:(id)sender {
     [self.syncResolveButton setSelected:YES];
     [self.asyncResolveButton setSelected:NO];
-    [self.syncResolveNonBlockingButton setSelected:NO];
+    [self.syncNonBlockingResolveButton setSelected:NO];
+
+    self.resolveTypeExplanation.text = @"使用同步解析接口解析域名，会阻塞当前线程，直到获得有效解析结果并返回。";
+    self.resolveType = ResolveTypeSync;
 }
 
 - (IBAction)asyncButton:(id)sender {
     [self.asyncResolveButton setSelected:YES];
     [self.syncResolveButton setSelected:NO];
-    [self.syncResolveNonBlockingButton setSelected:NO];
+    [self.syncNonBlockingResolveButton setSelected:NO];
+
+    self.resolveTypeExplanation.text = @"使用异步解析接口解析域名，不会阻塞当前线程，解析结果会通过回调的形式返回。";
+    self.resolveType = ResolveTypeAsync;
 }
 
 - (IBAction)syncNonBlockingButton:(id)sender {
-    [self.syncResolveNonBlockingButton setSelected:YES];
+    [self.syncNonBlockingResolveButton setSelected:YES];
     [self.asyncResolveButton setSelected:NO];
     [self.syncResolveButton setSelected:NO];
+
+    self.resolveTypeExplanation.text = @"使用同步非阻塞接口解析域名，不会阻塞当前线程，但可能会返回空结果。";
+    self.resolveType = ResolveTypeSyncNoBlocking;
+}
+
+- (IBAction)beginResolve:(id)sender {
+    ResolveFuctionViewController *resolveViewController = [HTTPDNSDemoTools storyBoardInstantiateViewController:@"ResolveFuctionViewController"];
+    [self.navigationController showViewController:resolveViewController sender:nil];
+    
+    // 延迟0.1s 等UI控件加载完成后再进行调用
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [resolveViewController resolveHost:self.domainTextField.text type:self.resolveType];
+    });
 }
 
 - (IBAction)chooseOrInputHost:(id)sender {
@@ -61,7 +84,7 @@
 #pragma mark - ChooseOrInputDomainDelegate
 
 - (void)domainResult:(NSString *)domain {
-    self.domainsTextField.text = domain;
+    self.domainTextField.text = domain;
     [self updateResolveButtonState];
 }
 
--- httpdns_ios_demo/httpdns_ios_demo/Fuction/ResolveFuctionViewController.h
+++ httpdns_ios_demo/httpdns_ios_demo/Fuction/ResolveFuctionViewController.h
@@ -7,10 +7,17 @@
 
 #import <UIKit/UIKit.h>
 
+typedef enum : NSUInteger {
+    ResolveTypeSync,
+    ResolveTypeAsync,
+    ResolveTypeSyncNoBlocking,
+} ResolveType;
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ResolveFuctionViewController : UIViewController
 
+- (void)resolveHost:(NSString *)host type:(ResolveType)resolveType;
 @end
 
 NS_ASSUME_NONNULL_END
