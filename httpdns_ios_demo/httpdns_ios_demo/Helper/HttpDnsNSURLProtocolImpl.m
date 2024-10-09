516a234cb206dcbf803085c8503e40233d9af120
yx456323 <yx456323@alibaba-inc.com>
2024-10-09 20:13:45 +0800

    feat: update HttpDnsNSURLProtocolImpl.m to support gzip chunk decompress

--- httpdns_ios_demo/httpdns_ios_demo/Helper/HttpDnsNSURLProtocolImpl.m
+++ httpdns_ios_demo/httpdns_ios_demo/Helper/HttpDnsNSURLProtocolImpl.m
@@ -19,10 +19,23 @@ static NSString *const kAnchorAlreadyAdded = @"AnchorAlreadyAdded";
 @property (strong, readwrite, nonatomic) NSMutableURLRequest *curRequest;
 @property (strong, readwrite, nonatomic) NSRunLoop *curRunLoop;
 @property (strong, readwrite, nonatomic) NSInputStream *inputStream;
+@property (assign, nonatomic) z_stream gzipStream;
 
 @end
 
 @implementation HttpDnsNSURLProtocolImpl
+
+- (instancetype)initWithRequest:(NSURLRequest *)request cachedResponse:(nullable NSCachedURLResponse *)cachedResponse client:(nullable id <NSURLProtocolClient>)client{
+    self = [super initWithRequest:request cachedResponse:cachedResponse client:client];
+    if (self) {
+        _gzipStream.zalloc = Z_NULL;
+        _gzipStream.zfree = Z_NULL;
+        if (inflateInit2(&_gzipStream, 16 + MAX_WBITS) != Z_OK) {
+            [self.client URLProtocol:self didFailWithError:[[NSError alloc] initWithDomain:@"gzip initialize fail" code:-1 userInfo:nil]];
+        }
+    }
+    return self;
+}
 
 /**
  *  是否拦截处理指定的请求
@@ -326,11 +339,13 @@ static NSString *const kAnchorAlreadyAdded = @"AnchorAlreadyAdded";
         }
     } else if (eventCode == NSStreamEventErrorOccurred) {
         [self closeStream:aStream];
+        inflateEnd(&_gzipStream);
         // 通知client发生错误了
         [self.client URLProtocol:self didFailWithError:
          [[NSError alloc] initWithDomain:@"NSStreamEventErrorOccurred" code:-1 userInfo:nil]];
     } else if (eventCode == NSStreamEventEndEncountered) {
         [self closeStream:_inputStream];
+        inflateEnd(&_gzipStream);
         [self.client URLProtocolDidFinishLoading:self];
     }
 }
@@ -407,45 +422,29 @@ static NSString *const kAnchorAlreadyAdded = @"AnchorAlreadyAdded";
     NSMutableData *decompressed = [NSMutableData dataWithLength:full_length + half_length];
     BOOL done = NO;
     int status;
+    _gzipStream.next_in = (Bytef *)[gzippedData bytes];
+    _gzipStream.avail_in = (uInt)[gzippedData length];
+    _gzipStream.total_out = 0;
 
-    z_stream strm;
-    strm.next_in = (Bytef *)[gzippedData bytes];
-    strm.avail_in = (uInt)[gzippedData length];
-    strm.total_out = 0;
-    strm.zalloc = Z_NULL;
-    strm.zfree = Z_NULL;
-
-    if (inflateInit2(&strm, 16 + MAX_WBITS) != Z_OK) {
-        return nil;
-    }
-
-    while (!done) {
-        if (strm.total_out >= [decompressed length]) {
+    while (_gzipStream.avail_in != 0 && !done) {
+        if (_gzipStream.total_out >= [decompressed length]) {
             [decompressed increaseLengthBy:half_length];
         }
 
-        strm.next_out = (Bytef *)[decompressed mutableBytes] + strm.total_out;
-        strm.avail_out = (uInt)([decompressed length] - strm.total_out);
+        _gzipStream.next_out = (Bytef *)[decompressed mutableBytes] + _gzipStream.total_out;
+        _gzipStream.avail_out = (uInt)([decompressed length] - _gzipStream.total_out);
 
-        status = inflate(&strm, Z_SYNC_FLUSH);
+        status = inflate(&_gzipStream, Z_SYNC_FLUSH);
 
         if (status == Z_STREAM_END) {
             done = YES;
         } else if (status != Z_OK) {
-            break;
+            return nil;
         }
     }
 
-    if (inflateEnd(&strm) != Z_OK) {
-        return nil;
-    }
-
-    if (done) {
-        [decompressed setLength:strm.total_out];
-        return [NSData dataWithData:decompressed];
-    } else {
-        return nil;
-    }
+    [decompressed setLength:_gzipStream.total_out];
+    return [NSData dataWithData:decompressed];
 }
 
 @end
